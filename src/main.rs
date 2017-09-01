aae2769c81087e81c119737abb7fb1cbf37ee443
Eric Tenza <ejtenza@gmail.com>
2017-09-01 08:44:52 -0500

    Math present and working; lower-numbers unaccounted for, needs to take input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 7;
+    let mut now: i32 = 0;
+    let mut last_last: i32 = 0;
+    let mut last: i32 = 1;
+
+    for elem in 2..(x + 1) {
+        now = last + last_last;
+        println!("{}: {} = {} + {}", elem, now, last, last_last);
+        last_last = last;
+        last = now;
+    }
 }
