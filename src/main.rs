d9a205112c888b112c74e9995021af04a0908640
Eric Tenza <ejtenza@gmail.com>
2017-09-01 12:32:07 -0400

    Add support for negafibonacci; Start encapsulating logic in smaller functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io; // bring the `io` library into scope
+use std::io;
 
 fn main() {
 
@@ -7,35 +7,44 @@ fn main() {
     loop {
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input)
-            .expect("failed to read line;"); // one line of code, two lines of text;
+            .expect("failed to read line;");
         let user_input: i64 = match user_input.trim().parse() {
             Ok(num) => num,
-            Err(_) => -1,
+            Err(_) => continue,
         };
 
         if user_input.signum() == -1 {
-            println!{"You must input a non-negative integer; please try again."};
-            continue;
+            let calc: i64 = calc_fib(user_input.wrapping_neg());
+            let answer: i64 = negafib_adjustment(user_input, calc);
+            println!("{}", answer);
+            break;
         } else {
-            let mut now: u64 = 0;
-            let mut last_last: u64 = 0;
-            let mut last: u64 = 1;
-
-            if user_input == 0 {
-                0
-            } else if user_input == 1 {
-                1
-            } else {
-                for _ in 2..(user_input + 1) {
-                    now = last + last_last;
-                    // println!("{}: {} = {} + {}", elem, now, last, last_last);
-                    last_last = last;
-                    last = now;
-                };
-                now
-            };
-            println!("Fibonacci #{} is {}", user_input, now);
+            let answer: i64 = calc_fib(user_input);
+            println!("{}", answer);
             break;
         }
     }
+}
+
+fn calc_fib(x: i64) -> i64 {
+    let mut now: i64 = 0;
+    let mut last_last: i64 = 0;
+    let mut last: i64 = 1;
+
+    if x == 0 {
+        now = 0;
+    } else if x == 1 {
+        now = 1;
+    } else {
+        for _ in 2..(x + 1) {
+            now = last + last_last;
+            last_last = last;
+            last = now;
+        };
+    }
+    now
+}
+
+fn negafib_adjustment(player_choice: i64, x: i64) -> i64 {
+    if player_choice % 2 == 0 { x.wrapping_neg() } else { x }
 }
